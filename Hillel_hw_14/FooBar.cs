75c7201d3f89b8614378edbb96f4a44dab64b4f5
Chertenag <chertenag@gmail.com>
2023-03-18 17:23:47 +0200

    Добавлено HW14.

---
+++ Hillel_hw_14/FooBar.cs
@@ -0,0 +1,40 @@
+﻿namespace Hillel_hw_14
+{
+    public static class FooBar
+    {
+        private static readonly AutoResetEvent autoReset1 = new(true);
+        private static readonly AutoResetEvent autoReset2 = new(false);
+
+        public static async Task Run(int count)
+        {
+            await FooBarToConsole(count);
+            Console.WriteLine();
+            Console.WriteLine($"End writing with {count} FooBar`s.");
+        }
+
+        private static async Task FooBarToConsole(int count)
+        {
+            await Task.WhenAll(Task.Run(() => Foo(count)), Task.Run(() => Bar(count)));
+        }
+
+        private static void Foo(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                autoReset1.WaitOne();
+                Console.Write("foo");
+                autoReset2.Set();
+            }
+        }
+
+        private static void Bar(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                autoReset2.WaitOne();
+                Console.Write("bar");
+                autoReset1.Set();
+            }
+        }
+    }
+}
---
+++ Hillel_hw_14/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Hillel_hw_14
+{
+    internal class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter number of FooBar`s to write:");
+                string? value = Console.ReadLine();
+                if (value != null)
+                {
+                    if (Int32.TryParse(value, out int count))
+                    {
+                        if (count < 0)
+                        {
+                            Console.WriteLine($"You can't count something negative number of times.");
+                        }
+                        else
+                        {
+                            await FooBar.Run(count);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{value} is not a number.");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
