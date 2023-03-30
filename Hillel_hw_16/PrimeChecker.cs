c233877265955dab09e780555e97d2d89d27411c
Chertenag <chertenag@gmail.com>
2023-03-30 22:03:23 +0300

    Добавлено HW16.

---
+++ Hillel_hw_16/PrimeChecker.cs
@@ -0,0 +1,36 @@
+﻿namespace Hillel_hw_16
+{
+    public static class PrimeChecker
+    {
+        /// <summary>
+        /// Проверка на простые числа. 
+        /// https://en.wikipedia.org/wiki/Primality_test
+        /// </summary>
+        /// <param name="value">Число для проверки.</param>
+        /// <returns>true если число простое.</returns>
+        public static bool Check(double value)
+        {
+            //Проверка на целочисельное
+            if (value != (int)value)
+                return false;
+
+            if (value == 2 || value == 3)
+            {
+                return true;
+            }
+
+            if (value <= 1 || value % 2 == 0 || value % 3 == 0)
+                return false;
+
+            for (int i = 5; i * i <= value; i += 6)
+            {
+                if (value % i == 0 || value % (i + 2) == 0)
+                    return false;
+            }
+            {
+                return true;
+            }
+        }
+    }
+
+}
---
+++ Hillel_hw_16/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Concurrent;
+
+namespace Hillel_hw_16
+{
+    internal class Program
+    {
+        private static BlockingCollection<int> collection = new();
+
+        static void Main(string[] _)
+        {
+            ParallePrimeCheck(100);
+            collection.ToList().ForEach(Console.WriteLine);
+        }
+
+        private static void ParallePrimeCheck(int n)
+        {
+            collection = new();
+            Parallel.For(0, n, AddToCollectionIfPrime);
+        }
+
+        private static void AddToCollectionIfPrime(int i)
+        {
+            if (PrimeChecker.Check(i))
+            {
+                collection.Add(i);
+            }
+        }
+    }
+
+}
