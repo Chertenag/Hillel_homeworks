14b5904a8c4a99688d92c539b809b7f1ecd189da
Chertenag <chertenag@gmail.com>
2023-05-14 20:13:41 +0300

    Добавлено HW27. TCP multiuser chat.

---
+++ Hillel_hw_27_client/Program.cs
@@ -0,0 +1,80 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Hillel_hw_27_client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TCPChatClient chatClient = new(16777);
+
+            while (true)
+            {
+                string message = Console.ReadLine();
+                if (String.IsNullOrEmpty(message))
+                {
+                    continue;
+                }
+                chatClient.SendMessage(message);
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class TCPChatClient
+    {
+        private TcpClient client;
+        private NetworkStream stream;
+        private byte[] buffer = new byte[1024];
+
+        public TCPChatClient(int port)
+        {
+            this.client = new TcpClient();
+            this.client.Connect(IPAddress.Loopback, port);
+            this.stream = this.client.GetStream();
+            this.stream.BeginRead(this.buffer, 0, this.buffer.Length, ClientRecieveCallback, null);
+        }
+
+        private void ClientRecieveCallback(IAsyncResult asyncResult)
+        {
+            try
+            {
+                var bytesReaded = stream.EndRead(asyncResult);
+                if (bytesReaded > 0)
+                {
+                    String str = DateTime.Now + ". " + Encoding.UTF8.GetString(this.buffer, 0, bytesReaded);
+                    Console.WriteLine(str);
+                }
+                buffer = new byte[1024];
+                this.stream.BeginRead(this.buffer, 0, this.buffer.Length, ClientRecieveCallback, null);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+        public void SendMessage(string message)
+        {
+            this.stream.Write(Encoding.UTF8.GetBytes(message));
+        }
+
+        public void Close()
+        {
+            if (this.stream != null)
+            {
+                this.stream.Close();
+                this.stream.Dispose();
+            }
+            if (this.client != null)
+            {
+                this.client.Close();
+                this.client.Dispose();
+            }
+        }
+
+    }
+
+}
---
+++ Hillel_hw_27_server/Program.cs
@@ -0,0 +1,121 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace Hillel_hw_27_server
+{
+    public delegate void PacketRespondSolver(string message, int sourceId);
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TCPChatServer chatServer = new();
+            chatServer.StartServer(16777);
+            Console.ReadLine();
+        }
+    }
+
+
+    public class TCPChatServer
+    {
+        private List<TCPChatServerClient> clients = new();
+        private TcpListener tcpListener;
+
+        public void StartServer(int port)
+        {
+            this.tcpListener = new TcpListener(IPAddress.Loopback, port);
+            this.tcpListener.Start();
+            this.tcpListener.BeginAcceptTcpClient(AcceptTCPClientCallback, null);
+            Console.WriteLine("Server started.");
+        }
+
+        private void AcceptTCPClientCallback(IAsyncResult asyncResult)
+        {
+            TCPChatServerClient client = new(this.tcpListener.EndAcceptTcpClient(asyncResult), BroadcastingMessage);
+            this.clients.Add(client);
+
+            string newUserMessage = $"New user ID:{client.Id} connected.";
+            Console.WriteLine(DateTime.Now.ToString() + ": " + newUserMessage);
+            BroadcastingMessage("SERVER : " + newUserMessage, client.Id);
+
+            this.tcpListener.BeginAcceptTcpClient(AcceptTCPClientCallback, null);
+        }
+
+        private void BroadcastingMessage(string message, int sourceId)
+        {
+            byte[] buffer = Encoding.UTF8.GetBytes(message);
+
+            this.clients.AsParallel()
+                .Where(c => c.Id != sourceId)
+                .ForAll(x =>
+            {
+                if (x.Client.Connected)
+                {
+                    x.Stream.Write(buffer, 0, buffer.Length);
+                }
+                else
+                {
+                    x.Close();
+                    this.clients.Remove(x);
+                }
+            });
+        }
+    }
+
+    public class TCPChatServerClient
+    {
+        public TcpClient Client;
+        public NetworkStream Stream;
+        public int Id;
+        private byte[] buffer = new byte[1024];
+        private PacketRespondSolver respondSolver;
+        protected static int nextID;
+
+        public TCPChatServerClient(TcpClient client, PacketRespondSolver packetRespondSolver)
+        {
+            Id = Interlocked.Increment(ref nextID);
+            respondSolver = packetRespondSolver;
+            Client = client;
+            Stream = client.GetStream();
+            Stream.BeginRead(this.buffer, 0, this.buffer.Length, ClientRecieveCallback, null);
+        }
+
+        private void ClientRecieveCallback(IAsyncResult asyncResult)
+        {
+            try
+            {
+                var bytesReaded = Stream.EndRead(asyncResult);
+                if (bytesReaded > 0)
+                {
+                    byte[] message = new byte[bytesReaded];
+                    Array.Copy(this.buffer, message, bytesReaded);
+                    var str = "ID (" + this.Id.ToString("00") + "): " + Encoding.UTF8.GetString(message);
+
+                    respondSolver(str, this.Id);
+                }
+                Stream.BeginRead(this.buffer, 0, this.buffer.Length, ClientRecieveCallback, null);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public void Close()
+        {
+            if (Stream != null)
+            {
+                Stream.Close();
+                Stream.Dispose();
+            }
+            if (Client != null)
+            {
+                Client.Close();
+                Client.Dispose();
+            }
+        }
+    }
+}
